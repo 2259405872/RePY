91c7117b29bf96a5ed56b91b92bf36f826acf391
杨山 <2259405872@qq.com>
2022-11-23 10:41:00 +0800

    first commit

---
+++ Basic/ex13.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script=argv
+
+print(f"The script is called:,{script}")
+#print(f"The first is called:,{first}")
+#print(f"The second is called:,{second}")
+#print(f"The third is called:,{third}")
+#print(f"The fourth is called:,{fourth}")
